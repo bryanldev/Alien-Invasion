7c77fe8b75e6c3afaddb7a881ce392e260b3699d
bryanlds <bryanlds5@gmail.com>
2018-05-24 14:08:37 -0300

    add a keyboard shortcut to end the game - q key

--- game_functions.py
+++ game_functions.py
@@ -14,6 +14,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_SPACE:
         fire_bullet(ai_settings, screen, ship, bullets)
 
+    elif event.key == pygame.K_q:
+        sys.exit()
+
 
 def check_keyup_events(event, ship):
     """Respond to key releases."""
