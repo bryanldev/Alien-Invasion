69d648640c1bb95d4e3faf41a156eab989d2dc8f
bryanlds <bryanlds5@gmail.com>
2018-07-23 18:58:57 -0300

    aliens and bottom of the screen collision

--- game_functions.py
+++ game_functions.py
@@ -99,6 +99,9 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 
+    # Look for aliens hitting the bottom of the screen.
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+
 
 def get_number_of_aliens_x(ai_settings, alien_width):
     """Determine the number of aliens that fit in a row."""
@@ -168,3 +171,13 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
 
     # Pause.
     sleep(0.5)
+
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Check if any aliens have reached the bottom of the screen."""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Treat this the same as if the ship got hit.
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
